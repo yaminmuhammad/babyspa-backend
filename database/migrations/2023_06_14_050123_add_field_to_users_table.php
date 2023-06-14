3724ec38ef407246270462da4afd2e0c00e66f87
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-06-14 12:13:00 +0700

    add migration service category & migration user

---
+++ database/migrations/2023_06_14_050123_add_field_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->after('name');
+            $table->string('address')->after('phone');
+            $table->string('city')->after('address');
+            $table->string('roles')->after('city');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+            $table->dropColumn('city');
+            $table->dropColumn('roles');
+        });
+    }
+};
