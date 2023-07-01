cd9b2c9b89ae43cdd41eb6a1117e219867e4e3a9
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-07-01 17:22:08 +0700

    init service form request

---
+++ app/Http/Controllers/ServiceCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ServiceCategory;
+use Illuminate\Http\Request;
+
+class ServiceCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ServiceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ServiceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ServiceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ServiceCategory $serviceCategory)
+    {
+        //
+    }
+}
