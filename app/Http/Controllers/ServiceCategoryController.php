c884bbc4d4842dffd58f5a737b2a8455051b8fd6
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-07-02 06:03:06 +0700

    feat CRUD

--- app/Http/Controllers/ServiceCategoryController.php
+++ app/Http/Controllers/ServiceCategoryController.php
@@ -23,7 +23,12 @@ class ServiceCategoryController extends Controller
                 <a class="inline-block border border-gray-700 text-red-600 rounded-md px-2 py-1 m-1 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline" 
                     href="' . route('dashboard.category.edit', $item->id) . '">
                     Edit
-                </a>';
+                </a><form class="inline-block" action="' . route('dashboard.category.destroy', $item->id) . '" method="POST">
+                <button class="border border-red-500 bg-red-500 text-white rounded-md px-2 py-1 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline" >
+                    Hapus
+                </button>
+                    ' . method_field('delete') . csrf_field() . '
+                </form>';
                 })
                 ->rawColumns(['action'])
                 ->make();
@@ -65,7 +70,9 @@ class ServiceCategoryController extends Controller
      */
     public function edit(ServiceCategory $category)
     {
-        //
+        return view('pages.dashboard.category.edit', [
+            'item' => $category
+        ]);
     }
 
     /**
@@ -73,7 +80,11 @@ class ServiceCategoryController extends Controller
      */
     public function update(ServiceCategoryRequest $request, ServiceCategory $category)
     {
-        //
+        $data = $request->all();
+
+        $category->update($data);
+
+        return redirect()->route('dashboard.category.index');
     }
 
     /**
@@ -81,6 +92,8 @@ class ServiceCategoryController extends Controller
      */
     public function destroy(ServiceCategory $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('dashboard.category.index');
     }
 }
